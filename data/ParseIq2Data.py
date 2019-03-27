c2c260d00335b4d70aad689ef0493a370b94e9a0
stoplime <slanimero@gmail.com>
2019-03-26 20:39:44 -0400

    Splits the iq2 data

--- data/ParseIq2Data.py
+++ data/ParseIq2Data.py
@@ -14,6 +14,8 @@ import copy
 
 PATH = os.path.abspath(os.path.dirname(__file__))
 
+val_split = 0.1
+test_split = 0.1
 filePath = os.path.join(PATH, "iq2_data_release.json")
 dataName = "iq2"
 outputFolders = {
@@ -49,10 +51,18 @@ def test():
                                 keys.append(__key)
                             # Selects the speaker
                             if __key == "speaker":
-                                print(__val)
+                                print("\t",__val)
+                                pass
                             # Selects the paragraphs
                             if __key == "paragraphs":
-                                print(__val[0])
+                                print()
+                                if len(__val) > 0:
+                                    for utterance in __val:
+                                        for sentences in parse_utterance2sentences(utterance):
+                                            # for sentence in sentences:
+                                            print("------")
+                                            print(sentences)
+                                            print("------")
     print(count)
     print(keys)
     # print(rawData)
@@ -79,16 +89,103 @@ def even_spliter(total, inRange=[250, 500]):
             splitRange[1] -= 1
     return [total]
 
+def parse_utterance2sentences(utterance):
+    sentences = []
+    sentence = ""
+    for letter in utterance:
+        end_sentence = False
+        sentence += letter
+        if len(sentence) > 1 and sentence[0] == " ":
+            sentence = sentence[1:]
+        if letter == "?" or letter == "!":
+            end_sentence = True
+        if letter == "…":
+            end_sentence = True
+            sentence = sentence[:-1]
+        if len(sentence) > 2 and sentence[-3:] == "...":
+            sentence = sentence.replace("...", ".")
+            end_sentence = True
+        if len(sentence) > 1 and sentence[-2:] == "--":
+            end_sentence = True
+        if len(sentence) > 2 and sentence[-2:] == ". " and sentence[-3].islower():
+            sentence = sentence.replace(". ", ".")
+            end_sentence = True
+
+        if end_sentence and len(sentence) > 2:
+            sentences.append(sentence.lower())
+            sentence = ""
+        elif end_sentence:
+            sentence = ""
+    if len(sentence) > 2:
+        sentences.append(sentence.lower())
+        sentence = ""
+    return sentences
+
+def data_iterator():
+    with open(filePath) as f:
+        rawData = json.load(f)
+
+    # dialogue structure
+    # List() - A dialogue per entry
+        # List() - A single dialogue
+            # Dictionary{} - The Utterance 
+            #              - "speaker": "person name"
+            #              - "sentences": List()
+                # "sentences" List() - A sentence per entry 
+    dialogues = []
+    for key, val in rawData.items():
+        dialog = []
+        for _key, _val in val.items():
+            # Select transcript
+            if _key == "transcript":
+                for transcript in _val:
+                    utterance = {}
+                    utterance["sentences"] = []
+                    # print(utterance["sentences"])
+                    # Go through the list of transcripts
+                    for __key, __val in transcript.items():
+                        # Selects the speaker
+                        if __key == "speaker":
+                            utterance["speaker"] = __val
+                            # print(__val)
+                        # Selects the paragraphs
+                        if __key == "paragraphs":
+                            for ut in __val:
+                                for sentences in parse_utterance2sentences(ut):
+                                    # print(type(sentences))
+                                    # print(utterance["sentences"])
+                                    utterance["sentences"].append(sentences)
+                                    # print(__val[0])
+                    dialog.append(utterance)
+        dialogues.append(dialog)
+    return dialogues
+
+def dialogues2listSentences(dialogues):
+    speakers = {}
+    sentences = []
+    for dialog in dialogues:
+        dialog_sentences = []
+        for utterance in dialog:
+            if utterance["speaker"] not in list(speakers.keys()):
+                speakers[utterance["speaker"]] = len(speakers)
+            for sentence in utterance["sentences"]:
+                dialog_sentences.append(sentence + " %$* " + str(speakers[utterance["speaker"]]))
+        sentences.append(dialog_sentences)
+    # print(speakers)
+    return sentences, speakers
+
 def main():
     # test()
-    print(even_spliter(50))
-    print(even_spliter(500))
-    print(even_spliter(501))
-    print(even_spliter(502))
-    print(even_spliter(1000))
-    print(even_spliter(1001))
-    print(even_spliter(999))
-    print(even_spliter(1250))
+    # pass
+    # test_utterance = "Apparently the debate continues even as you cast your votes. And, and after—after we tabulate, I know your question, it’s a good one, and we will, we will pose it, I wanna thank the debaters and the audience, uh, for their participation. And we have to take care of some housekeeping here, the next Intelligence Squared debate will be on Tuesday, February 12th, here at Asia Society and Museum. The motion to be debated then is, “America should be the world’s policeman.” It will be moderated by “60 Minutes” correspondent Morley Safer. The panelists for the next debate are, for the motion, Senior Fellow for National Security Studies at the Council on Foreign Relations Max Boot, Professor and Director of the American Foreign Policy Program at Johns Hopkins University, Michael Mandelbaum, and author and director of the Centre for Social Cohesion, Douglas The Rosenkranz Foundation - Intelligence Squared US Debate “Performance Enhancing Drugs in Competitive Sports” Murray. Against the motion, president and founder of Eurasia Group Ian Bremer, President and CEO of the Henry L. Stimson Center, Ellen Laipson, and Matthew Parris, writer for the Times of London and broadcaster for the BBC. An edited version of tonight’s Intelligence Squared debate can be heard locally on WNYC-AM 820, on Sunday, January 27th, at 8 p.m. These debates are also heard on more than 90 NPR stations across the country, please check your local NPR member station listings for the dates and times of broadcast, outside New York City. Copies of Dick Pound’s books, Inside Dope: How Drugs Are the Biggest Threat to Sport, Why You Should Care and What Should Be Done About Them, and Inside the Olympics: A Behind the Scenes Look at the Politics, the Scandals, and the Glory of the Games, are on sale upstairs in the lobby, you can also purchase DVD’s from previous debates here tonight, or from the Intelligence Squared website. And now, very, very briefly, should there be two tiers of sports, Olympics or otherwise, one for those who use, one for those who don’t, one on this side, one on that side, 10 seconds. Should there be two tiers?"
+    # sentences = parse_utterance2sentences(test_utterance)
+    # print(len(sentences))
+    # for sentence in sentences:
+    #     print(sentence)
+    #     print("------")
+    data = data_iterator()
+    sentences, speakers = dialogues2listSentences(data)
+    print(len(sentences[0]))
 
 if __name__ == "__main__":
     main()
